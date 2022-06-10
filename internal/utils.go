c90bfc467327eab0fd6328c036d321e73c258f9e
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-06-10 13:54:34 -0700

    Removed unused functions

--- internal/utils.go
+++ internal/utils.go
@@ -7,15 +7,12 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
-	"strings"
 	"time"
 )
 
@@ -90,88 +87,6 @@ func DirExists(path string) bool {
 		}
 	}
 	return info.IsDir()
-}
-
-// Attempt to copy a ``src`` file to a ``dst`` destination.
-// Reference: https://blog.depa.do/post/copy-files-and-directories-in-go
-func CopyFile(src, dst string) error {
-	var err error
-	var srcfd *os.File
-	var dstfd *os.File
-	var srcinfo os.FileInfo
-
-	if srcfd, err = os.Open(src); err != nil {
-		return err
-	}
-	defer srcfd.Close()
-
-	if dstfd, err = os.Create(dst); err != nil {
-		return err
-	}
-	defer dstfd.Close()
-
-	if _, err = io.Copy(dstfd, srcfd); err != nil {
-		return err
-	}
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-	return os.Chmod(dst, srcinfo.Mode())
-}
-
-// Attempt to copy a ``src`` directory to a ``dst`` destination.
-// Reference: https://blog.depa.do/post/copy-files-and-directories-in-go
-func CopyDir(src string, dst string) error {
-	var err error
-	var fds []os.FileInfo
-	var srcinfo os.FileInfo
-
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-
-	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
-		return err
-	}
-
-	if fds, err = ioutil.ReadDir(src); err != nil {
-		return err
-	}
-	for _, fd := range fds {
-		srcfp := path.Join(src, fd.Name())
-		dstfp := path.Join(dst, fd.Name())
-
-		if fd.IsDir() {
-			if err = CopyDir(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			if err = CopyFile(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-	return nil
-}
-
-// Ask user for confirmation and return a boolean.
-// Reference: https://gist.github.com/r0l1/3dcbb0c8f6cfe9c66ab8008f55f8f28b
-func AskConfirm(prompt string) bool {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Printf("%s [y/n]: ", prompt)
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("[-] Failed to read user input\n")
-			return false
-		}
-		input = strings.ToLower(strings.TrimSpace(input))
-		if input == "y" || input == "yes" {
-			return true
-		} else if input == "n" || input == "no" {
-			return false
-		}
-	}
 }
 
 // Check the $PATH environment variable for a given ``cmd`` and return a ``bool``
