c17e05ff31b00f41a2c5f30d39fac58b4f0962ce
Christopher Maddalena <chris.maddalena@protonmail.com>
2022-07-14 15:42:27 -0700

    Added ``testify`` package for unit tests

--- cmd/internal/env_test.go
+++ cmd/internal/env_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"github.com/stretchr/testify/assert"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -12,69 +13,72 @@ func TestGhostwriterEnvironmentVariables(t *testing.T) {
 	// Test parsing values and writing to the .env file
 	env_file := filepath.Join(GetCwdFromExe(), ".env")
 	ParseGhostwriterEnvironmentVariables()
-	if !FileExists(env_file) {
-		t.Error("Expected `.env` file to be created")
-	}
+	assert.True(t, FileExists(env_file), "Expected .env file to exist")
 
 	// Test a default value
-	if ghostEnv.Get("django_date_format") != "d M Y" {
-		t.Errorf("Expected `django_date_format` to be `d M Y`, got %s", ghostEnv.Get("django_date_format"))
-	}
+	assert.Equal(t, ghostEnv.Get("django_date_format"), "d M Y", "Value of `django_date_format` should be `d M Y`")
 
 	// Test modifying the .env file for production mode
 	SetProductionMode()
-	if ghostEnv.GetBool("hasura_graphql_dev_mode") {
-		t.Error("Expected `hasura_graphql_dev_mode` to be false, got true")
-	}
-	if !ghostEnv.GetBool("django_secure_ssl_redirect") {
-		t.Error("Expected `django_secure_ssl_redirect` to be true, got false")
-	}
-	if ghostEnv.GetBool("hasura_graphql_enable_console") {
-		t.Error("Expected `hasura_graphql_enable_console` to be false, got true")
-	}
-	if ghostEnv.GetBool("hasura_graphql_insecure_skip_tls_verify") {
-		t.Error("Expected `hasura_graphql_insecure_skip_tls_verify` to be false, got true")
-	}
-	if ghostEnv.Get("django_settings_module") != "config.settings.production" {
-		t.Errorf("Expected `django_settings_module` to be `config.settings.production`, got %s", ghostEnv.Get("django_settings_module"))
-	}
+	assert.Equal(t, ghostEnv.GetBool("hasura_graphql_dev_mode"), false, "Production value of `hasura_graphql_dev_mode` should be false")
+	assert.Equal(t, ghostEnv.GetBool("django_secure_ssl_redirect"), true, "Production value of `django_secure_ssl_redirect` should be true")
+	assert.Equal(t, ghostEnv.GetBool("hasura_graphql_enable_console"), false, "Production value of `hasura_graphql_enable_console` should be false")
+	assert.Equal(t, ghostEnv.GetBool("hasura_graphql_insecure_skip_tls_verify"), false, "Production value of `hasura_graphql_insecure_skip_tls_verify` should be false")
+	assert.Equal(t, ghostEnv.GetString("django_settings_module"), "config.settings.production", "Production value of `django_settings_module` should be `config.settings.production`")
 
 	// Test modifying the .env file for dev mode
 	SetDevMode()
-	if !ghostEnv.GetBool("hasura_graphql_dev_mode") {
-		t.Error("Expected `hasura_graphql_dev_mode` to be true, got false")
-	}
-	if ghostEnv.GetBool("django_secure_ssl_redirect") {
-		t.Error("Expected `django_secure_ssl_redirect` to be false, got true")
-	}
-	if !ghostEnv.GetBool("hasura_graphql_enable_console") {
-		t.Error("Expected `hasura_graphql_enable_console` to be true, got false")
-	}
-	if !ghostEnv.GetBool("hasura_graphql_insecure_skip_tls_verify") {
-		t.Error("Expected `hasura_graphql_insecure_skip_tls_verify` to be true, got false")
-	}
-	if ghostEnv.Get("django_settings_module") != "config.settings.local" {
-		t.Errorf("Expected `django_settings_module` to be `config.settings.local`, got %s", ghostEnv.Get("django_settings_module"))
-	}
+	assert.Equal(t, ghostEnv.GetBool("hasura_graphql_dev_mode"), true, "Development value of `hasura_graphql_dev_mode` should be true")
+	assert.Equal(t, ghostEnv.GetBool("django_secure_ssl_redirect"), false, "Development value of `django_secure_ssl_redirect` should be false")
+	assert.Equal(t, ghostEnv.GetBool("hasura_graphql_enable_console"), true, "Development value of `hasura_graphql_enable_console` should be true")
+	assert.Equal(t, ghostEnv.GetBool("hasura_graphql_insecure_skip_tls_verify"), true, "Development value of `hasura_graphql_insecure_skip_tls_verify` should be true")
+	assert.Equal(t, ghostEnv.GetString("django_settings_module"), "config.settings.local", "Development value of `django_settings_module` should be `config.settings.local`")
 
 	// Test ``GetConfig()``
-	GetConfig([]string{"postgres_password", "django_date_format"})
+	format := GetConfig([]string{"django_compress_enabled", "django_date_format"})
+	assert.Equal(
+		t,
+		format,
+		Configurations(Configurations{Configuration{Key: "django_compress_enabled", Val: "true"}, Configuration{Key: "django_date_format", Val: "d M Y"}}),
+		"`GetConfig()` should return a Configurations object",
+	)
+	assert.Equal(t, len(format), 2, "`GetConfig()` with two valid variables should return a two values")
+
+	// Test ``GetConfigAll()``
+	config := GetConfigAll()
+	assert.Equal(t, len(config), 46, "`GetConfigAll()` should return all values")
 
 	// Test ``SetConfig()``
 	SetConfig("django_date_format", "Y M d")
-	if ghostEnv.Get("django_date_format") != "Y M d" {
-		t.Errorf("Set `django_date_format` to `Y M d`, got %s instead", ghostEnv.Get("django_date_format"))
-	}
+	assert.Equal(t, ghostEnv.GetString("django_date_format"), "Y M d", "Default value of `django_date_format` should be `Y M d`")
 
 	// Test ``AllowHost()``
 	AllowHost("test.local")
-	if !strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "test.local") {
-		t.Errorf("Expected `django_allowed_hosts` to contain `test.local`, got %s", ghostEnv.Get("django_allowed_hosts"))
-	}
+	assert.True(t, strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "test.local"), "Value of `django_allowed_hosts` should include `test.local`")
+	AllowHost("test.local")
+	assert.Equal(t, strings.Count(ghostEnv.GetString("django_allowed_hosts"), "test.local"), 1, "Value of `django_allowed_hosts` should include only one entry for `test.local`")
+	AllowHost("ghostwriter.local")
+	assert.True(t, strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "ghostwriter.local"), "Value of `django_allowed_hosts` should include `ghostwriter.local`")
 
 	// Test ``DisallowHosts()``
 	DisallowHost("test.local")
-	if strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "test.local") {
-		t.Errorf("Expected `django_allowed_hosts` to NOT contain `test.local`, got %s", ghostEnv.Get("django_allowed_hosts"))
-	}
+	assert.False(t, strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "test.local"), "Value of `django_allowed_hosts` should no longer include `test.local`")
+	assert.True(t, strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "ghostwriter.local"), "Value of `django_allowed_hosts` should still include `ghostwriter.local`")
+	DisallowHost("ghostwriter.local")
+	assert.False(t, strings.Contains(ghostEnv.GetString("django_allowed_hosts"), "ghostwriter.local"), "Value of `django_allowed_hosts` should no longer include `ghostwriter.local`")
+
+	// Test ``TrustOrigin()``
+	TrustOrigin("test.local")
+	assert.True(t, strings.Contains(ghostEnv.GetString("django_csrf_trusted_origins"), "test.local"), "Value of `django_csrf_trusted_origins` should include `test.local`")
+	TrustOrigin("test.local")
+	assert.Equal(t, strings.Count(ghostEnv.GetString("django_csrf_trusted_origins"), "test.local"), 1, "Value of `django_csrf_trusted_origins` should include only one entry for `test.local`")
+	TrustOrigin("ghostwriter.local")
+	assert.True(t, strings.Contains(ghostEnv.GetString("django_csrf_trusted_origins"), "ghostwriter.local"), "Value of `django_csrf_trusted_origins` should include `ghostwriter.local`")
+
+	// Test ``DistrustOrigin()``
+	DistrustOrigin("test.local")
+	assert.False(t, strings.Contains(ghostEnv.GetString("django_csrf_trusted_origins"), "test.local"), "Value of `django_csrf_trusted_origins` should include `test.local`")
+	assert.True(t, strings.Contains(ghostEnv.GetString("django_csrf_trusted_origins"), "ghostwriter.local"), "Value of `django_csrf_trusted_origins` should still include `ghostwriter.local`")
+	DistrustOrigin("ghostwriter.local")
+	assert.False(t, strings.Contains(ghostEnv.GetString("django_csrf_trusted_origins"), "ghostwriter.local"), "Value of `django_csrf_trusted_origins` should include `ghostwriter.local`")
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/spf13/viper v1.10.1
 require (
 	github.com/Luzifer/go-dhparam v1.1.0 // indirect
 	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/docker v20.10.15+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
@@ -21,16 +22,20 @@ require (
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/cobra v1.4.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
