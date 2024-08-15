6bdb460473727b6efa41121c97cae310e368b0ec
Christopher Maddalena <chris.maddalena@protonmail.com>
2024-08-15 15:27:22 -0700

    Added SSO config options to the env

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.20] - 2024-08-15
+
+### Added
+
+* Added configuration options for the new SSO options coming to Ghostwriter
+  * Options are `django_social_account_domain_allowlist` and `django_social_account_login_on_get`
+
 ## [0.2.19] - 2024-03-29
 
 ### Changed
--- cmd/internal/env.go
+++ cmd/internal/env.go
@@ -71,6 +71,8 @@ func setGhostwriterConfigDefaultValues() {
 	ghostEnv.SetDefault("django_session_save_every_request", true)
 	ghostEnv.SetDefault("django_settings_module", "config.settings.local")
 	ghostEnv.SetDefault("django_social_account_allow_registration", false)
+	ghostEnv.SetDefault("django_social_account_domain_allowlist", "")
+	ghostEnv.SetDefault("django_social_account_login_on_get", false)
 	ghostEnv.SetDefault("django_superuser_email", "admin@ghostwriter.local")
 	ghostEnv.SetDefault("django_superuser_password", GenerateRandomPassword(32, true))
 	ghostEnv.SetDefault("django_superuser_username", "admin")
