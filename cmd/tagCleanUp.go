338a28cc3c2a86971f3d78dee5df2e22f9c3ef73
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-10-23 13:45:50 -0700

    Code clean up for readability

--- cmd/tagCleanUp.go
+++ cmd/tagCleanUp.go
@@ -27,24 +27,22 @@ func init() {
 }
 
 func tagCleanUp(cmd *cobra.Command, args []string) {
+	var yamlFile string
+
 	docker.EvaluateDockerComposeStatus()
 	if dev {
 		fmt.Println("[+] Executing tag cleanup in the development environment...")
 		docker.SetDevMode()
-		docker.RunManagementCmd("local.yml", "deduplicate_tags")
-		c := docker.AskForConfirmation("[?] Do you want to also remove orphaned tags?")
-		if !c {
-			os.Exit(0)
-		}
-		docker.RunManagementCmd("local.yml", "remove_orphaned_tags")
+		yamlFile = "local.yml"
 	} else {
 		fmt.Println("[+] Executing tag cleanup in the production environment...")
 		docker.SetProductionMode()
-		docker.RunManagementCmd("local.yml", "deduplicate_tags")
-		c := docker.AskForConfirmation("[?] Do you want to also remove orphaned tags?")
-		if !c {
-			os.Exit(0)
-		}
-		docker.RunManagementCmd("production.yml", "remove_orphaned_tags")
+		yamlFile = "production.yml"
 	}
+	docker.RunManagementCmd(yamlFile, "deduplicate_tags")
+	c := docker.AskForConfirmation("[?] Do you want to also remove orphaned tags?")
+	if !c {
+		os.Exit(0)
+	}
+	docker.RunManagementCmd(yamlFile, "remove_orphaned_tags")
 }
