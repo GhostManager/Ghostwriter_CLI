48e11d5cac569827152a79fda8b6cee9179cb2a4
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-10-23 13:45:40 -0700

    Updated to use `RunManagementCmd`

--- cmd/migrateTotp.go
+++ cmd/migrateTotp.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 
 	docker "github.com/GhostManager/Ghostwriter_CLI/cmd/internal"
@@ -12,21 +11,21 @@ import (
 
 var migrateTotpCmd = &cobra.Command{
 	Use:   "migrate_totp",
-	Short: "Migrate TOTP secrets and migration codes from Ghostwriter v1 to v2",
-	Long: `This command migrates TOTP secrets and migration codes from a Ghostwriter v1 installation to a Ghostwriter v2 installation.
-It reads the TOTP secrets and migration codes from the v1 database and updates the corresponding user records in the v2 database.
+	Short: "Migrate TOTP secrets and migration codes from Ghostwriter <=v6 to v6.1+",
+	Long: `This command migrates TOTP secrets and migration codes from an installation of Ghostwriter v6.0 or earlier to a Ghostwriter v6.1 or later installation.
+It reads the TOTP secrets and migration codes from the database and updates the corresponding user records.
 `,
 	Run: migrateTotp,
 }
-
 
 func init() {
 	rootCmd.AddCommand(migrateTotpCmd)
 }
 
 func migrateTotp(cmd *cobra.Command, args []string) {
-	docker.EvaluateDockerComposeStatus()
 	var yamlFile string
+
+	docker.EvaluateDockerComposeStatus()
 	if dev {
 		docker.SetDevMode()
 		yamlFile = "local.yml"
@@ -35,21 +34,15 @@ func migrateTotp(cmd *cobra.Command, args []string) {
 		yamlFile = "production.yml"
 	}
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Migrating TOTP secrets and migration codes from Ghostwriter v1 to v2.\n")
+	fmt.Printf("Migrating TOTP secrets and migration codes from Ghostwriter <=v6 to v6.1+.\n")
 	fmt.Print("Press enter to continue, or Ctrl+C to cancel\n")
 	reader.ReadString('\n')
 
 	docker.RunDockerComposeDown(yamlFile, false)
 	fmt.Println("[+] migrating TOTP secrets and migration codes")
 
-	err := docker.RunCmd("docker", []string{"compose", "-f", yamlFile, "run", "--rm", "django", "python", "manage.py", "migrate"})
-	if err != nil {
-		log.Fatalf("Error occurred while running migrate: %v", err)
-	}
-	err = docker.RunCmd("docker", []string{"compose", "-f", yamlFile, "run", "--rm", "django", "python", "manage.py", "migrate_totp_device"})
-	if err != nil {
-		log.Fatalf("Error occurred while running migrate_totp_device: %v", err)
-	}
+	docker.RunManagementCmd(yamlFile, "migrate")
+	docker.RunManagementCmd(yamlFile, "migrate_totp_device")
 
 	fmt.Println("[+] TOTP secrets and migration codes migration complete")
 }
