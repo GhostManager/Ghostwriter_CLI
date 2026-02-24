bf9a87eef88cce0f5ba116d5cc6ac6dd187c8923
Christopher Maddalena <chris.maddalena@protonmail.com>
2026-02-24 09:58:18 -0800

    Adjusted so env is restored if there is an error

--- cmd/test.go
+++ cmd/test.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 
 	internal "github.com/GhostManager/Ghostwriter_CLI/cmd/internal"
 	"github.com/spf13/cobra"
@@ -15,14 +14,14 @@ var testCmd = &cobra.Command{
 	Long: `Runs Ghostwriter's unit tests in the development environment.
 
 Requires to "install --mode=local-dev" to have been run first.`,
-	Run: runUnitTests,
+	RunE: runUnitTests,
 }
 
 func init() {
 	rootCmd.AddCommand(testCmd)
 }
 
-func runUnitTests(cmd *cobra.Command, args []string) {
+func runUnitTests(cmd *cobra.Command, args []string) error {
 	dockerInterface := internal.GetDockerInterface(mode)
 	dockerInterface.Env.Save()
 	fmt.Println("[+] Running Ghostwriter's unit and integration tests...")
@@ -30,6 +29,13 @@ func runUnitTests(cmd *cobra.Command, args []string) {
 	// Save the current env values we're about to change
 	currentActionSecret := dockerInterface.Env.Get("HASURA_GRAPHQL_ACTION_SECRET")
 	currentSettingsModule := dockerInterface.Env.Get("DJANGO_SETTINGS_MODULE")
+
+	// Defer restoration to ensure it happens even if tests fail
+	defer func() {
+		dockerInterface.Env.Set("HASURA_GRAPHQL_ACTION_SECRET", currentActionSecret)
+		dockerInterface.Env.Set("DJANGO_SETTINGS_MODULE", currentSettingsModule)
+		dockerInterface.Env.Save()
+	}()
 
 	// Change env values for the test conditions
 	dockerInterface.Env.Set("HASURA_GRAPHQL_ACTION_SECRET", "changeme")
@@ -39,11 +45,8 @@ func runUnitTests(cmd *cobra.Command, args []string) {
 	// Run the unit tests
 	testErr := dockerInterface.RunDjangoManageCommand("test")
 	if testErr != nil {
-		log.Fatalf("Error trying to run Ghostwriter's tests: %v\n", testErr)
+		return fmt.Errorf("error trying to run Ghostwriter's tests: %w", testErr)
 	}
 
-	// Reset the changed env values
-	dockerInterface.Env.Set("HASURA_GRAPHQL_ACTION_SECRET", currentActionSecret)
-	dockerInterface.Env.Set("DJANGO_SETTINGS_MODULE", currentSettingsModule)
-	dockerInterface.Env.Save()
+	return nil
 }
